85467ae6f1a7024cd12a2ff2f965a26b82fc1a01
uielPark <parkuiery@gmail.com>
2024-04-28 19:03:06 +0900

    feat :: network 세팅

---
+++ app/src/main/java/com/uiel/scul/network/Retrofit.kt
@@ -0,0 +1,22 @@
+package com.uiel.scul.network
+
+import com.uiel.scul.BuildConfig
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object Retrofit {
+    private val okHttpClient = OkHttpClient.Builder()
+        .addInterceptor(
+            HttpLoggingInterceptor()
+                .setLevel(HttpLoggingInterceptor.Level.BODY)
+        )
+        .build()
+
+    private val retrofit = Retrofit.Builder()
+        .baseUrl(BuildConfig.BASE_URL)
+        .client(okHttpClient)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+}
