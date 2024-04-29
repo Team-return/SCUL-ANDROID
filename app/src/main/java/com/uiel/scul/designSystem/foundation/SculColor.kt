c666f9a5b23bfd9dc1644fea9fd244efd423b7bf
uielPark <parkuiery@gmail.com>
2024-04-29 21:18:09 +0900

    feat :: SculColor

---
+++ app/src/main/java/com/uiel/scul/designSystem/foundation/SculColor.kt
@@ -0,0 +1,49 @@
+package com.uiel.scul.designSystem.foundation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import com.uiel.scul.R
+
+object SculColor{
+    val WHITE
+        @Composable get() = colorResource(id = R.color.white)
+    val BLACK
+        @Composable get() = colorResource(id = R.color.black)
+    val GRAY50
+        @Composable get() = colorResource(id = R.color.gray_50)
+    val GRAY100
+        @Composable get() = colorResource(id = R.color.gray_100)
+    val GRAY200
+        @Composable get() = colorResource(id = R.color.gray_200)
+    val GRAY300
+        @Composable get() = colorResource(id = R.color.gray_300)
+    val GRAY400
+        @Composable get() = colorResource(id = R.color.gray_400)
+    val GRAY500
+        @Composable get() = colorResource(id = R.color.gray_500)
+    val GRAY600
+        @Composable get() = colorResource(id = R.color.gray_600)
+    val GRAY700
+        @Composable get() = colorResource(id = R.color.gray_700)
+    val GRAY800
+        @Composable get() = colorResource(id = R.color.gray_800)
+    val GRAY900
+        @Composable get() = colorResource(id = R.color.gray_900)
+    val MAIN100
+        @Composable get() = colorResource(id = R.color.main_100)
+    val MAIN200
+        @Composable get() = colorResource(id = R.color.main_200)
+    val MAIN300
+        @Composable get() = colorResource(id = R.color.main_300)
+    val MAIN400
+        @Composable get() = colorResource(id = R.color.main_400)
+    val MAIN500
+        @Composable get() = colorResource(id = R.color.main_500)
+    val MAIN600
+        @Composable get() = colorResource(id = R.color.main_600)
+    val MAIN700
+        @Composable get() = colorResource(id = R.color.main_700)
+    val MAIN800
+        @Composable get() = colorResource(id = R.color.main_800)
+}
