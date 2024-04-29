322c89f2826533ff9b89cf6db3ce20ce7af778b9
uielPark <parkuiery@gmail.com>
2024-04-29 20:12:29 +0900

    chore :: 라이브러리 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -87,6 +87,8 @@ dependencies {
     implementation("androidx.navigation:navigation-compose:2.7.7")
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
-    implementation("com.squareup.okhttp3:okhttp:4.10.0")
-    implementation("com.squareup.okhttp3:logging-interceptor:4.10.0")
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
+    implementation("io.coil-kt:coil-compose:2.5.0")
+    implementation ("com.github.skydoves:landscapist-coil:1.4.7")
 }
