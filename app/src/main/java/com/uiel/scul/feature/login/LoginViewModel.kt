39b4779c046a1f0cd1c5bab60aa36926cbacd9b9
uiel <parkuiery@gmail.com>
2024-05-02 23:09:56 +0900

    refactor :: LoginViewModel

--- app/src/main/java/com/uiel/scul/feature/login/LoginViewModel.kt
+++ app/src/main/java/com/uiel/scul/feature/login/LoginViewModel.kt
@@ -1,5 +1,7 @@
 package com.uiel.scul.feature.login
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.uiel.scul.model.user.LoginRequest
@@ -9,11 +11,21 @@ import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.launch
 
-class LoginViewModel : ViewModel(){
+class LoginViewModel : ViewModel() {
     private val usersApi = Retrofit.usersApi
+    //private val usersApi = Retrofit.retrofit.create(UsersApi::)
 
     private val _event = MutableSharedFlow<Event>()
     val event = _event.asSharedFlow()
+
+    private val _accountId = MutableLiveData("")
+    val accountId: LiveData<String> = _accountId
+
+    private val _password = MutableLiveData("")
+    val password: LiveData<String> = _password
+
+    private val _buttonEnabled = MutableLiveData(false)
+    val buttonEnabled: LiveData<Boolean> = _buttonEnabled
 
     fun login(
         accountId: String,
@@ -30,6 +42,12 @@ class LoginViewModel : ViewModel(){
             _event.emit(Event.NavigateToMain)
         }.onFailure {
 
+        }
+    }
+
+    private fun checkInput() {
+        if(_accountId.value.isNotEmpty() && _password.value.isNotEmpty()) {
+            _buttonEnabled.value
         }
     }
 
