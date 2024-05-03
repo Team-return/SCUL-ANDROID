965e3157eb2f5b4e974135e26146cc666fb5fc28
uiel <parkuiery@gmail.com>
2024-05-03 12:26:52 +0900

    feat :: icon

---
+++ app/src/main/java/com/uiel/scul/designSystem/foundation/SculIcon.kt
@@ -0,0 +1,9 @@
+package com.uiel.scul.designSystem.foundation
+
+import com.uiel.scul.R
+
+object SculIcon {
+    val Search = R.drawable.ic_search
+    val MyPage = R.drawable.ic_mypage
+    val Home = R.drawable.ic_home
+}
