280fae040904fb5e095726c89f485247a544f14c
uiel <parkuiery@gmail.com>
2024-05-04 18:23:49 +0900

    refactor :: 코드 정리

--- app/src/main/java/com/uiel/scul/feature/login/LoginViewModel.kt
+++ app/src/main/java/com/uiel/scul/feature/login/LoginViewModel.kt
@@ -1,9 +1,8 @@
 package com.uiel.scul.feature.login
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.uiel.scul.SculApplication
 import com.uiel.scul.model.user.LoginRequest
 import com.uiel.scul.network.Retrofit
 import kotlinx.coroutines.Dispatchers
@@ -13,19 +12,9 @@ import kotlinx.coroutines.launch
 
 class LoginViewModel : ViewModel() {
     private val usersApi = Retrofit.usersApi
-    //private val usersApi = Retrofit.retrofit.create(UsersApi::)
 
     private val _event = MutableSharedFlow<Event>()
     val event = _event.asSharedFlow()
-
-    private val _accountId = MutableLiveData("")
-    val accountId: LiveData<String> = _accountId
-
-    private val _password = MutableLiveData("")
-    val password: LiveData<String> = _password
-
-    private val _buttonEnabled = MutableLiveData(false)
-    val buttonEnabled: LiveData<Boolean> = _buttonEnabled
 
     fun login(
         accountId: String,
@@ -39,15 +28,10 @@ class LoginViewModel : ViewModel() {
                 )
             )
         }.onSuccess {
+            SculApplication.preferences.setString("AccessToken","Bearer ${it.accessToken}")
             _event.emit(Event.NavigateToMain)
         }.onFailure {
 
-        }
-    }
-
-    private fun checkInput() {
-        if(_accountId.value.isNotEmpty() && _password.value.isNotEmpty()) {
-            _buttonEnabled.value
         }
     }
 
