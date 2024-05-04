306875cf6411f07ba521ecd25a2a5f96d3d2a60e
uiel <parkuiery@gmail.com>
2024-05-04 18:24:29 +0900

    feat :: SignupScreen

---
+++ app/src/main/java/com/uiel/scul/feature/signup/SignupScreen.kt
@@ -0,0 +1,11 @@
+package com.uiel.scul.feature.signup
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun SignupScreen(
+    navController: NavController,
+) {
+
+}
