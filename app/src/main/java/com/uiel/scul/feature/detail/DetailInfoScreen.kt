994888a66c150b1ce920c6ba38b9cdb75bd486cc
uiel <parkuiery@gmail.com>
2024-05-04 18:21:55 +0900

    feat :: screen 추가

---
+++ app/src/main/java/com/uiel/scul/feature/detail/DetailInfoScreen.kt
@@ -0,0 +1,9 @@
+package com.uiel.scul.feature.detail
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun DetailInfoScreen() {
+    Text(text = "sdfds")
+}
