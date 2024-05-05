50974c1275bc65754978d0ed14ecf0c0076b246e
uiel <parkuiery@gmail.com>
2024-05-05 23:05:31 +0900

    refactor :: 상세 화면 추가

--- app/src/main/java/com/uiel/scul/SculApp.kt
+++ app/src/main/java/com/uiel/scul/SculApp.kt
@@ -1,9 +1,11 @@
 package com.uiel.scul
 
 import androidx.compose.runtime.Composable
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.uiel.scul.feature.detail.DetailScreen
 import com.uiel.scul.feature.login.LoginScreen
 import com.uiel.scul.feature.login.LoginViewModel
@@ -24,8 +26,16 @@ fun SculApp() {
         composable("main") {
             BottomNavigationBar(navController = navController)
         }
-        composable("detail") {
-            DetailScreen(navController = navController)
+        composable(
+            route = "detail/{cultureId}",
+            arguments = listOf(navArgument("cultureId") {
+                type = NavType.StringType
+            })
+        ) {
+            DetailScreen(
+                cultureId = it.arguments?.getString("cultureId") ?: "",
+                navController = navController,
+            )
         }
     }
 }
