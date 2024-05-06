76af773fc8e702efa624629c4bb2ce463cdeff3c
uiel <parkuiery@gmail.com>
2024-05-06 15:49:34 +0900

    chore :: 버전 변경

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "com.uiel.scul"
         minSdk = 26
         targetSdk = 34
-        versionCode = 3
-        versionName = "1.0.2"
+        versionCode = 4
+        versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
