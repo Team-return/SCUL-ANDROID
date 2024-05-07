867a1a095d81e2bd041a11625a3723920bbaa687
uielPark <parkuiery@gmail.com>
2024-05-07 23:15:12 +0900

    feat :: my review

---
+++ app/src/main/java/com/uiel/scul/model/myreview/FetchMyReviewsResponse.kt
@@ -0,0 +1,15 @@
+package com.uiel.scul.model.myreview
+
+import java.util.UUID
+
+data class FetchMyReviewsResponse(
+    val reviewList: List<FetchMyReviewResponse>
+
+)
+
+data class FetchMyReviewResponse(
+    val id: UUID,
+    val writer: String,
+    val content: String,
+    val createdAt: String,
+)
