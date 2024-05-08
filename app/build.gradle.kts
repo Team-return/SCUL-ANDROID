0e861bd21ac64311dc6c4905e263958d483065ef
uielPark <parkuiery@gmail.com>
2024-05-08 22:51:49 +0900

    🚀 :: v1.1.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "com.uiel.scul"
         minSdk = 26
         targetSdk = 34
-        versionCode = 6
-        versionName = "1.1.2"
+        versionCode = 7
+        versionName = "1.1.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
