5194243817e30f665319aa1bb49b1753b6f8b3d0
uielPark <parkuiery@gmail.com>
2024-05-08 22:52:02 +0900

    refactor :: 상태바 생상 변경

--- app/src/main/java/com/uiel/scul/MainActivity.kt
+++ app/src/main/java/com/uiel/scul/MainActivity.kt
@@ -3,11 +3,14 @@ package com.uiel.scul
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.core.view.WindowCompat
 import com.uiel.scul.ui.theme.SCULTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window,false)
+
        setContent {
            SCULTheme {
                SculApp()
