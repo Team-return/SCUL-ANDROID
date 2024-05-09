6a19c328f422993806ff51152d34d8193b95d571
uielPark <parkuiery@gmail.com>
2024-05-09 21:16:42 +0900

    refactor :: scroll 추가

--- app/src/main/java/com/uiel/scul/feature/detail/DetailInfoScreen.kt
+++ app/src/main/java/com/uiel/scul/feature/detail/DetailInfoScreen.kt
@@ -5,6 +5,8 @@ import android.net.Uri
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.scrollable
+import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -13,6 +15,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
@@ -50,6 +53,7 @@ fun DetailInfoScreen(
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(20.dp)
+                .horizontalScroll(rememberScrollState()),
         ) {
             uiState.wantedPeople.split(",").apply {
                 this.forEach {
@@ -67,6 +71,7 @@ fun DetailInfoScreen(
                         text = it,
                         style = SculTypography.Label2,
                         color = SculColor.BLACK,
+                        maxLines = 1,
                     )
                     Spacer(modifier = Modifier.width(12.dp))
                 }
